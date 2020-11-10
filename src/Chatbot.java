189c5d2f17e8042753db06bf6254119a24a6118b
Haruki Oshiro <e205721@ie.u-ryukyu.ac.jp>
2020-11-10 10:04:06 +0900

    done

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -11,4 +11,8 @@ public class Chatbot{
     public void greeting(){
         System.out.println("はじめまして" + this.name + "です。よろ！");
     }
+
+    public void setName(String _name){
+        this.name = _name;
+    }
 }
